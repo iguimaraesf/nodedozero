eca52f6627595bf850065036c963ff799d7a0e50
Ivan Guimarães Farias <iguimaraesf@gmail.com>
2022-11-22 12:01:17 -0300

    feature/capitulo2

--- server/index.js
+++ server/index.js
@@ -3,6 +3,11 @@ const app = express()
 
 const db = require('./models')
 
+// rotas
+const postRouter = require('./routes/Posts')
+app.use(express.json())
+app.use('/posts', postRouter)
+
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {
         console.log('Servidor na porta 3001')
